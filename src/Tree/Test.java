05d7c6d503896491f6e5b5a99bb935854ceaa7b1
anh1704 <truongngocanh17042004@gmail.com>
2023-05-10 14:16:59 +0700

    echo "# Vidu" >> git add git init

---
+++ src/Tree/Test.java
@@ -0,0 +1,10 @@
+package Tree;
+
+public class Test {
+    public static void main(String[] args) {
+        MyBinaryTree myBinaryTree = new MyBinaryTree();
+        myBinaryTree.init();
+
+        System.out.println("Done");
+    }
+}
